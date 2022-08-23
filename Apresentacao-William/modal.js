81b8ddc09d2cf847c7543c62d336b489cab56bc8
WilliamPorto22 <williamporto0@gmail.com>
2022-08-23 18:48:57 -0300

    Atualizacao

---
+++ Apresentacao-William/modal.js
@@ -0,0 +1,30 @@
+const modal = document.querySelector('.modal-container')
+const modal2 = document.querySelector('.modal-container-2')
+const modal3 = document.querySelector('.modal-container-3')
+const modal4 = document.querySelector('.modal-container-4')
+
+function openModal(){
+    modal.classList.add ('active')
+}
+function openModal2(){
+    modal2.classList.add ('active')
+}
+function openModal3(){
+    modal3.classList.add ('active')
+}
+function openModal4(){
+    modal4.classList.add ('active')
+}
+
+function closeModal(){
+    modal.classList.remove ('active')
+}
+function closeModal2(){
+    modal2.classList.remove ('active')
+}
+function closeModal3(){
+    modal3.classList.remove ('active')
+}
+function closeModal4(){
+    modal4.classList.remove ('active')
+}
